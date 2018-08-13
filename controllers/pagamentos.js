8dbf66890c1ea6b2d6f067ee5485461dec4a5540
denissoliveira <denissoliveira@gmail.com>
2018-08-13 16:05:04 -0300

    Confirmação e cancelamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,9 +1,12 @@
 module.exports = function(app){
+
+    //Teste
     app.get('/pagamentos', function(req, res){
       console.log('Recebida requisicao de teste na porta 3000.')
       res.send('OK.');
     });
   
+    //Cria um Pagamento
     app.post('/pagamentos/pagamento', function(req, res){
   
       req.assert("forma_de_pagamento",
@@ -34,13 +37,59 @@ module.exports = function(app){
           console.log('Erro ao inserir no banco:' + erro);
           res.status(500).send(erro);
         } else {
-        console.log('pagamento criado');
-        res.location('/pagamentos/pagamento/' +
-              resultado.insertId);
-  
-        res.status(201).json(pagamento);
-      }
+          console.log('Pagamento criado');
+          res.location('/pagamentos/pagamento/' +
+                resultado.insertId);
+    
+          res.status(201).json(pagamento);
+        }
       });
   
     });
+
+    //Atualização, consfirma um pagamento !!!
+    app.put('/pagamentos/pagamento/:id', function(req, res){
+
+      var pagamento = {};
+      var id = req.params.id;
+  
+      pagamento.id = id;
+      pagamento.status = 'CONFIRMADO';
+  
+      var connection = app.persistencia.connectionFactory();
+      var pagamentoDao = new app.persistencia.PagamentoDAO(connection);
+  
+      pagamentoDao.atualiza(pagamento, function(erro){
+          if (erro){
+            res.status(500).send(erro);
+            return;
+          }
+          console.log('Pagamento Confirmado');
+          res.send(pagamento);
+      });
+  
+    });
+
+    //Deleta um pagamento !!!
+    app.delete('/pagamentos/pagamento/:id', function(req, res){
+      var pagamento = {};
+      var id = req.params.id;
+  
+      pagamento.id = id;
+      pagamento.status = 'CANCELADO';
+  
+      var connection = app.persistencia.connectionFactory();
+      var pagamentoDao = new app.persistencia.PagamentoDAO(connection);
+  
+      pagamentoDao.atualiza(pagamento, function(erro){
+          if (erro){
+            res.status(500).send(erro);
+            return;
+          }
+          console.log('Pagamento cancelado');
+          res.status(204).send(pagamento);
+      });
+    });
+
+
   }
--- persistencia/connectionFactory.js
+++ persistencia/connectionFactory.js
@@ -2,7 +2,7 @@ var mysql  = require('mysql');
 
 function createDBConnection(){
     return mysql.createConnection({
-        host: '172.17.0.2',
+        host: '192.168.99.100',
         user: 'root',
         password: 'mysql',
         database: 'payfast'
